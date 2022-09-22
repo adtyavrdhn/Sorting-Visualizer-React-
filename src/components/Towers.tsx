e809925be6d744baa5fbcc428a64e5c507bbe52c
Aditya Vardhan <adtyavrdhn@gmail.com>
2022-09-22 14:39:16 +0530

    updated towers
    towers visible, height still not perfect

--- src/components/Towers.tsx
+++ src/components/Towers.tsx
@@ -4,10 +4,12 @@ import logo from "./logo.svg";
 function Towers(props: any) {
   const arr = props.arr;
   const towers = arr.map((n: number) => (
-    <div className={"h-" + n + " w-10px text-black"} key={n}></div>
+    <div className={"h-" + n + "px w-10px bg-black"} key={n}>
+      {n}
+    </div>
   ));
 
-  return <div className="TowerContainer">{towers}</div>;
+  return <div className="TowerContainer flex gap-10px">{towers}</div>;
 }
 
 export default Towers;
