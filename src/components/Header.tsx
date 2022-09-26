28d81f0e4363c123ec2e146eb0ce544ebf634e04
Aditya Vardhan <adtyavrdhn@gmail.com>
2022-09-26 18:02:49 +0530

    button added effects + styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,7 +38,7 @@ function Header(props: ChangeInput) {
         <SortBtn sortingAlgo={"Selection"} sort={props.sort}></SortBtn>
         <SortBtn sortingAlgo={"Quick"} sort={props.sort}></SortBtn>
         <button
-          className="shadow-md rounded-lg p-3 bg-purple-50"
+          className="inline-block px-6 py-2.5 bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
           onClick={() => props.allSort()}
         >
           All
--- src/components/SortButton.tsx
+++ src/components/SortButton.tsx
@@ -8,7 +8,7 @@ interface SortButtonProps {
 function SortButton(props: SortButtonProps) {
   return (
     <button
-      className="shadow-md rounded-lg p-3 bg-purple-50"
+      className="inline-block px-6 py-2.5 bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gradient-to-br focus:ring-2 dark:focus:ring-pink-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
       onClick={() => props.sort(`${props.sortingAlgo} Sort`)}
     >
       {props.sortingAlgo} Sort
