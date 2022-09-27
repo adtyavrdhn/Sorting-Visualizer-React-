3fe6b1c74e7c36c17afb79be6387f6efaab931bf
Aditya Vardhan <adtyavrdhn@gmail.com>
2022-09-27 23:57:02 +0530

    Update bubbleSort.ts

--- src/algorithms/bubbleSort.ts
+++ src/algorithms/bubbleSort.ts
@@ -6,19 +6,20 @@ export default async function bubbleSort(
   speed: number
 ) {
   const size = arr.length;
+
   console.log("Bubble Sort");
 
   const array: number[] = arr;
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size - i - 1; j++) {
-      await delay(speed);
+      await delay(15);
       if (array[j] > array[j + 1]) {
-        await delay(speed);
+        await delay(15);
         swap(array, j, j + 1);
         setArr([...array]);
       }
     }
-    await delay(speed);
+    await delay(5);
   }
   arr = [...array];
   return arr;
